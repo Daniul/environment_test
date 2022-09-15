d9ccce14a964938a545933b7a3d1e834839b2a52
danieldavid0901@gmail.com <60299227+Daniul@users.noreply.github.com>
2022-09-14 20:57:36 -0500

    completed Book Collection part 1 tasks

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,9 +4,16 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'Harry Potter'
+    fill_in 'Author', with: 'J.K. Rowling'
+    fill_in 'Price', with: '13'
+    fill_in 'Published date', with: '1997-06-26'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('Harry Potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content('13')
+    expect(page).to have_content('1997-06-26')
   end
+
 end
