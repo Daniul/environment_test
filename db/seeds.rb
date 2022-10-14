b2037d005f0b67db9b1cd05bd89065b603b05e0e
danieldavid0901@gmail.com <60299227+Daniul@users.noreply.github.com>
2022-10-13 21:48:47 -0500

    added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+  {title: "Harry Potta", author: "ME", price: "23.44", published_date: "2022-10-04"},
+  {title: "this is ", author: "cool man", price: "2.0", published_date: "2013-06-03"},
+  {title: "Red Deserts", author: "John Books", price: "23.22", published_date: "2018-01-01"}
+])
